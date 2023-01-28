4a16c19c250004292781a711904d77720d768539
raguileoam <raguileoam@gmail.com>
2023-01-28 12:43:43 -0300

    first commit

---
+++ src/main/java/com/raguileoam/virtualticket/VirtualTicketApplication.java
@@ -0,0 +1,13 @@
+package com.raguileoam.virtualticket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VirtualTicketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VirtualTicketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/raguileoam/virtualticket/VirtualTicketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.raguileoam.virtualticket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VirtualTicketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
